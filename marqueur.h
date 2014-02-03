fe2a4801a8fcc7cecceb63e88da1e9a7cb6df9a2
Johndufduf <jonathan.dufetel@gmail.com>
2014-02-03 11:15:43 +0100

    Renommage de Truc en marqueur

--- marqueur.h
+++ marqueur.h
@@ -1,16 +1,27 @@
+<<<<<<< HEAD:marqueur.h
+#ifndef Marqueur_h
+#define Marqueur_h
+=======
 #ifndef TRUC_H
 #define TRUC_H
+>>>>>>> DepotManu/master:truc.h
 
-class Truc
+class Marqueur
 {
 private:
 	int valeur;
 	char orientation;
 public:
+<<<<<<< HEAD:marqueur.h
+	Marqueur();
+	Marqueur(int val);
+	Marqueur(int val, char ori);
+=======
 	Truc();
 	Truc(int val);
 	Truc(int val, char ori);
 	Truc(const Truc& nvT); //Constructeur par recopie
+>>>>>>> DepotManu/master:truc.h
 	
 	int getValeur()const;
 	char getOrientation()const;
